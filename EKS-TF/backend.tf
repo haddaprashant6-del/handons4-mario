9fe566880f4e1224cb819a8e27112306797a422e
haddaprashant6-del <haddaprashant6@gmail.com>
2026-05-09 23:31:47 +0530

    test12

---
+++ EKS-TF/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "test321431"  # Ensure this is your actual S3 bucket name
+    key            = "eks/terraform.tfstate"  # Standardized directory naming
+    region         = "us-east-1"  # Changed to match provider region
+    encrypt        = true  # Ensures state file is encrypted
+    #dynamodb_table = "Tf-lock"  # Enables state locking to prevent conflicts
+  }
+}
